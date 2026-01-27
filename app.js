08715fff94ef8eba279f4d7436f692712d7aed1e
bhadaitmisal1991-del <bhadaitmisal1991@gmail.com>
2026-01-27 18:24:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -943,6 +943,6 @@ app.get('/api/getIpAdd', function(req, res) {
 });
 	
 // Binding express app to port 3000
-app.listen(3000,function(){
+app.listen(3306,function(){
     console.log('Node server running @ http://localhost:3000')
 });
