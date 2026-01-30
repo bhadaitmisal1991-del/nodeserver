125499cf9518feaeaad26c77dc27242b5f0c4b0c
bhadaitmisal1991-del <bhadaitmisal1991@gmail.com>
2026-01-30 11:51:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
 var connection = mysql.createConnection({
-    host : 'http://sql306.infinityfree.com',
-    user : 'if0_40975998',
-    password : 'Bhadait123',
-    database : 'if0_40975998_bhadaitmisal'	
+    host : 'bhadaitmisal-bhadaitmisal.h.aivencloud.com',
+    user : 'avnadmin',
+    password : 'AVNS_ZuGpR80Ew8TMU5YUCvl',
+    database : 'defaultdb'	
 });
 connection.connect();
 // ***** GET Items DATA ******
