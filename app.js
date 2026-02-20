f815a849328ff5c82a0f03950cdaf35b9c35751b
bhadaitmisal1991-del <bhadaitmisal1991@gmail.com>
2026-02-20 22:47:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.post('/api/login', (req, res) => {
         if (err || results.length === 0) return res.status(401).send('User not found');
 
         const user = results[0];
+		console.log(user.password+" "+password);
+		console.log(" valid "+bcrypt.compareSync(password, user.password));
         const passwordIsValid = bcrypt.compareSync(password, user.password);
 
         if (!passwordIsValid) return res.status(401).send('Invalid password');
