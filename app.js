7ad9cbf2ae457edc0c148ed6df41704105569c15
bhadaitmisal1991-del <bhadaitmisal1991@gmail.com>
2026-02-25 23:57:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -163,11 +163,22 @@ connection.query('INSERT INTO items SET ?', req.body, function(err, result) {
 // ***** GET BillNo ******
  app.get('/api/billno', function(req, res) {  
 var tmpdate = req.query.date; 
-       connection.query("SELECT * FROM bills where date='"+tmpdate+"' and tableno=0 ORDER BY billno DESC LIMIT 1",function(err, result){
+       connection.query("SELECT * FROM bills where date='"+tmpdate+"' and tableno=0 ORDER BY billno DESC LIMIT 1",function(err, result1){
              if (err){
                 res.send(err);
                 console.log(err);
              }
+					 //**
+					 connection.query("SELECT * FROM bills where date='"+tmpdate+"' ORDER BY billno DESC LIMIT 1",function(err, result2){
+					 if (err){
+						res.send(err);
+						console.log(err);
+					 }			 
+					 res.send({ data1: results1, data2: results2 });
+					 //res.json(result)
+					})
+					//****
+			 
              res.json(result)
             })  
 });	
